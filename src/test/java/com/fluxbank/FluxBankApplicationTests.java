8027eb5c48e3ae0625a3ed0b59fbd9a694dd1a47
Gag Hakobyan <gaghakobyan707@gmail.com>
2026-05-06 18:45:40 +0400

    Initial commit

---
+++ src/test/java/com/fluxbank/FluxBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
